e9e095d03ba98485a09408d12fd7234223bf6ba7
Ernesto Ledesma <eeledesma@teco.com.ar>
2025-08-11 20:20:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+let nombre = "Ernesto";
+let edad = 30;
+let profesion = "Desarrollador Web";
+
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Profesión: " + profesion);
