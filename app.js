25a3e126464d5e4c887dedea3cad140957e3f8ca
Ernesto Ledesma <eeledesma@teco.com.ar>
2025-08-27 16:59:34 -0300

    [ADD]:fetch api

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 
 
 
-let nombre = "Ernesto";
-let edad = 30;
-let profesion = "Desarrollador Web";
+// let nombre = "Ernesto";
+// let edad = 30;
+// let profesion = "Desarrollador Web";
 
-console.log("Nombre: " + nombre);
-console.log("Edad: " + edad);
-console.log("Profesión: " + profesion);
+// console.log("Nombre: " + nombre);
+// console.log("Edad: " + edad);
+// console.log("Profesión: " + profesion);
+
+function pick(obj, keys) {
+  const out = {};
+  for (const k of keys) {
+    if (Object.prototype.hasOwnProperty.call(obj, k)) {
+      out[k] = obj[k];
+    }
+  }
+  return out;
+}
+
